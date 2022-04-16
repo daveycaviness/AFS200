a803418e3d8a381767a3ee9c21a02920a8b82fdd
daveycaviness <85713610+daveycaviness@users.noreply.github.com>
2022-04-16 14:57:22 -0400

    AFS-200 Yahtzee

--- Week4/yahtzee/yahtzee.py
+++ Week4/yahtzee/yahtzee.py
@@ -1,27 +1,30 @@
 import random
 diceList = []
-Yahtzee = False
+Yahtzee = True
 
 # Create a class for a die (single dice)
 # Define any necessary attributes.  Your die class should be able to handle any number of sides such as 4, 6, 8, 10, 12, 20, 100, etc.
 # Create the following methods for the class:
 # __init__ - Constructor method
 class Die:
-    def __init__(self, sides, faceNum):
+    def __init__(self, sides = 6, faceNum = 1):
         self.sides = sides
         self.faceNum = faceNum
     
 # roll - Roll the die to get a new random value based on the number of sides of the die
     def roll(self):
-        self.getCurrentFaceValue(random.randint(1, self.sides))
+        self.setCurrentFaceValue(random.randint(1, self.sides))
 
 # getCurrentFaceValue - Get the value currently showing on the die face
-    def getCurrentFaceValue(self, newRoll):
+    def setCurrentFaceValue(self, newRoll):
         self.faceNum = newRoll
+
+    def getCurrentFaceValue(self):
+        return self.faceNum
 
 # showDieFace - Display the value currently showing on the die.
     def showDieFace(self):
-        return(self.faceNum)
+        print(f"({self.faceNum})")
  
 # Create a function that rolls five different dice.
 # Display the roll results to the screen
@@ -32,27 +35,25 @@ def diceRoll():
     die4 = Die(6, 1)
     die5 = Die(6, 1)
     
-    myDice = [die1, die2, die3, die4, die5]
+    myDice = [Die(6, 1), Die(6, 1), Die(6, 1), Die(6, 1), Die(6, 1)]
     
     for rolled in myDice:    
         rolled.roll()
-        diceList.append(rolled.showDieFace())
+        rolled.showDieFace()
 
 # Checks to see if all five dice have the same face value. If they do, then print “YAHTZEE” to the screen.    
-    for dice in diceList:
+    for dice in myDice:
         global Yahtzee
-        if diceList[0] != dice:
+        if myDice[0].getCurrentFaceValue() != dice.getCurrentFaceValue():
             Yahtzee = False
             break
         else: 
             Yahtzee = True
  
     if Yahtzee == False:
-        print(diceList)
         print("No Yahtzee. Continue rolling your dice.")
 
     else:
-        print(diceList)
         print("Yahtzee")
 
 
